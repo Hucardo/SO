5df6394bfd1b7383f1be36bd1661e0ee6e3ca315
Ricardo <ricardo.alexandre.antunes@ua.pt>
2023-10-26 20:47:13 +0100

    funçao deve funcionar agora

--- espaco1.sh
+++ espaco1.sh
@@ -10,10 +10,11 @@ function espaco() {
     fi
 
     for i in "$dir"/*; do
-        if [[ -f "$i" ]]; then
-            space=$(du -h "$i" | awk '{print $1}' | grep -oE '[0-9.]+')
-        elif [[ -d "$i" ]]; then
+        if [[ ! -d "$i" ]]; then
+            space=$(du "$i" | awk '{print $1}' | grep -oE '[0-9.]+')
+        else
             espaco "$i"
+	    total_var=$(( $total_var + 4 ))
         fi
         total_var=$(echo "$total_var + $space" | bc)
     done
@@ -21,15 +22,15 @@ function espaco() {
 
 # Usage example
 total_var=0
-espaco ~/SO/aula01
+espaco ../../AED
 total1=$total_var
 total_var=0
-espaco ~/SO/aula02
+espaco ../../MPEI
 total2=$total_var
 total_var=0
-espaco ~/SO
+espaco ../../SO
 total3=$total_var
 
-echo "Total Disk Usage for aula01: $total1"
-echo "Total Disk Usage for aula02: $total2"
+echo "Total Disk Usage for AED: $total1"
+echo "Total Disk Usage for MPEI: $total2"
 echo "Total Disk Usage for SO: $total3"
