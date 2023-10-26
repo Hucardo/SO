3018f5dbb3f9161a9d97fba351072f9a4f93251a
Ricardo <ricardo.alexandre.antunes@ua.pt>
2023-10-26 21:33:48 +0100

    mais fix (mais ou menos)

--- espaco1.sh
+++ espaco1.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
-
+shopt -s extglob
 function espaco() {
     local dir="$1"
     local space=0
-
+    echo $1
+    echo dirstart
     if [[ ! -d "$dir" ]]; then
         echo "Erro: Diretório inválido"
         return 1
     fi
-
-    for i in "$dir"/*; do
-        if [[ ! -d "$i" ]]; then
-            space=$(du "$i" | awk '{print $1}' | grep -oE '[0-9.]+')
-        else
-            espaco "$i"
-	    total_var=$(( $total_var + 4 ))
-        fi
-        total_var=$(echo "$total_var + $space" | bc)
+    directories=($(find "$dir" -mindepth 1 -maxdepth 1 -type d))
+    echo directories
+    return 0
+    for i in "${directories[@]}"; do
+	echo good
+	if [[ ! "$i" -ef "$dir/.[^.]*" ]] ; then
+		echo good
+		echo $i
+	        if [[ ! -d "$i" ]]; then
+	            space=$(du "$i" | awk '{print $1}' | grep -oE '[0-9.]+')
+	        else
+	            espaco "$i"
+	        fi
+	        total_var=$(echo "$total_var + $space" | bc)
+	fi
     done
 }
 
-# Usage example
+#Testes
 total_var=0
 espaco ../../AED
 total1=$total_var
