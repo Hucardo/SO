be12ede8cbce2fe5eec36505a1eabad617e8f85a
Ricardo <ricardo.alexandre.antunes@ua.pt>
2023-10-27 12:54:07 +0100

    eu sou gay

--- espaco3.sh
+++ espaco3.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
-shopt -s extglob
+
+flag_d=$(date -d $(date "+%b %d %H:%M") +%s)
+flag_n="*"
+flag_s=0
+flag_r=0
+flag_a=0
+flag_l=0
+
+
+
+while getopts ":d:n:r:a:s:l:" opt; do
+    case $opt in
+        d)
+            flag_d=$(date -d "$OPTARG" +%s)
+            ;;
+        n)
+            flag_n="$OPTARG"
+            ;;
+        r)
+            # Handle option -r if needed
+            ;;
+        a)
+            # Handle option -a if needed
+            ;;
+        s)
+            # Handle option -s if needed
+            ;;
+        l)
+            # Handle option -l if needed
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            ;;
+    esac
+done
+
+echo "SIZE NAME $(date +%Y%m%d) $@"
+
 function espaco() {
     local dir="$1"
     local space=0
@@ -7,27 +47,35 @@ function espaco() {
         echo "Erro: Diretório inválido"
         return 1
     fi
-    files=($(find "$dir" -type f))
+    files=($(find "$dir" -maxdepth 1 -type f -name "$flag_n" ! -newermt "@$flag_d"))
     for j in "${files[@]}"; do
         if [[ ! -d "$j" ]]; then
             space=$(du "$j" | awk '{print $1}' | grep -oE '[0-9.]+')
-	    echo $j
-	    echo $space
         fi
         total_var=$(echo "$total_var + $space" | bc)
-	echo $total_var
     done
 }
 
+function subespaco(){
+	dirs=($(find "$1" -type d))
+	for i in "${dirs[@]}"; do
+		total_var=0
+		espaco $i
+		lista[$i]=$total_var
+	done
+}
 
+function print() {
+	for i in "${!lista[@]}" ; do
+        	echo "${lista[$i]} $i"
+	done
+}
+
+declare -A lista
 #Testes
-total_var=0
-espaco teste01
-total1=$total_var
-total_var=0
-espaco teste02
-total2=$total_var
+for l in "$@"; do
+	subespaco $l
+done
 
-echo "Total Disk Usage for aula01: $total1"
-echo "Total Disk Usage for aula02: $total2"
+
 
