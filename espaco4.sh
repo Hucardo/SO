298b59b1df6f32ca3fcb043d04559444d7c97b02
Ricardo <ricardo.alexandre.antunes@ua.pt>
2023-10-28 17:46:29 +0100

    arranjado espaco4.sh

--- espaco4.sh
+++ espaco4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-start_time=$(date +%s)
+
 
 #valores por predefinição das flags
 flag_d=$(date +%s) #HOJE
@@ -52,7 +52,7 @@ function espaco() {
     fi
     dirs=($(find "$dir" -mindepth 1 -maxdepth 1 -type d))
     files=($(find "$dir" -maxdepth 1 -type f -name "$flag_n" ! -newermt "@$flag_d")) #encontra os ficheiros em $dir com o nome a corresponder a $flag_n alterados não depois de $Flag_d
-    for j in "${files[@]}"; do #itera sobre a lista de ficheiros encontrados
+    for j in "${files[@]}"; do #itera sobre a dict de ficheiros encontrados
         space=$(du "$j" | awk '{print $1}' | grep -oE '[0-9.]+') #encontra o tamanho do ficheiro usando du
         if [[ $space -ge $flag_s ]] ; then #verifica se o tamanho do ficheiro encontra os requisitos de tamanho
             total_var=$(echo "$total_var + $space" | bc) #soma o espaço do ficheiro analisado ao total até agora
@@ -64,21 +64,50 @@ function espaco() {
         espaco $k
         total_var=$(( $temp_var + $total_var ))
     done
-    lista[$dir]=$total_var
+    dict[$dir]=$total_var
 }
 
-function print() {
+function ordenador(){
+    #Completar para nao calcular arreis desnecessariamente
+    #basicamente mete aqui um if com as flags
+    #podes mudar o nome dos arrais todos para o mesmo nome porque so 1 vai ser calculado
+    #
+    #
+    #
+    #
+    ordered=($(for i in "${!dict[@]}"; do
+        done | sort -k2,2nr | cut -d' ' -f1))
+
+    reversed=($(for i in "${!dict[@]}"; do
+        done | sort -k2,2n | cut -d' ' -f1))
+
+    alphabetic=($(for i in "${!dict[@]}"; do
+        done | sort))
+
+    reversed_alphabetic=($(for i in "${!dict[@]}"; do
+        done | sort -r))
+}
+
+function printer() {
     count=1
-	for i in "${!lista[@]}" ; do
+	for i in "${!dict[@]}" ; do
 		if [[ ! $count -gt $flag_l ]] || [[ $flag_l -eq 0 ]] ; then
-        	echo "${lista[$i]} $i"
+        	echo "${dict[$i]} $i"
             count=$(( $count + 1 ))
         fi
 	done
 }
+#apagar os arrays desnecessarios
+#
+#
+#
 
+declare -A dict
+declare -a ordered
+declare -a reversed
+declare -a alphabetic
+declare -a reversed_alphabetic
 
-declare -A lista
 #Testes
 for l in "$@"; do
     if [[ -d "$l" ]]; then
@@ -86,7 +115,5 @@ for l in "$@"; do
 	    espaco $l
     fi
 done
-print
-end_time=$(date +%s)
-elapsed_time=$(($end_time - $start_time))
-echo "Elapsed time: $elapsed_time seconds"
+printer
+
