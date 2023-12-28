259a3843481aaf75bd85212d314119900e7b132e
Ricardo <ricardo.alexandre.antunes@ua.pt>
2023-12-28 16:34:31 +0000

    pasta

---
+++ sop_tg1_113402_115243/spacecheck.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+
+if (( $# == 0 )); then
+    echo "Erro: Nenhum argumento especificado"
+    exit 1
+fi
+
+#valores por predefinição das flags
+flag_d=$(date +%s) #HOJE
+flag_n="*" #TODOS OS FICHEIROS
+flag_s=0 #>=0 kbs
+flag_r=0 #Ordem decrescente
+flag_a="1,1n" #Sem ordenação por nome
+flag_l=0 #Sem limite de linhas
+
+while getopts "d:n:ras:l:" opt 2>/dev/null; do
+    case $opt in
+        d)
+            flag_d=$(date -d "$OPTARG" +%s 2>/dev/null) #Data especificada (No formato M d HH:MM)
+            if [[ -z $flag_d ]]; then
+                echo "Formato de data inválido"
+                exit 1
+            fi
+            ;;
+        n)
+            flag_n="$OPTARG" #ficheiros com o padrão especificado
+            if [[ "${flag_n:0:1}" == "-" ]]; then
+                echo "-n requere uma string (pattern)"
+                exit 1
+            fi
+            ;;
+        r)
+            flag_r=1 #Ordem crescente
+            ;;
+        a)
+            flag_a="2,1000" #Com ordenação por nome
+            ;;
+        s)
+            flag_s="$OPTARG" #>=flag_s kbs
+            if [[ ! $flag_s =~ ^[0-9]+$ ]]; then
+                echo "-s requer um número."
+                exit 1
+            fi
+            ;;
+        l)
+            flag_l="$OPTARG" #Com limite de linhas flag_l
+            if [[ ! $flag_l =~ ^[0-9]+$ ]]; then
+                echo "-l requer um número."
+                exit 1
+            fi
+            ;;
+        \?)
+            echo "A flag '-$OPTARG' é inválida (flags válidas são -d, -n, -r, -a, -s e -l)"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$flag_d" ]] || [[ -z "$flag_n" ]] || [[ -z "$flag_s" ]] || [[ -z "$flag_l" ]]; then
+    echo "Alguns argumentos para as flags -d, -n, -s ou -l são inexistentes ou inválidos."
+    exit 1
+fi
+
+r="r"
+if [[ $flag_a == "1,1n" && $flag_r == 1 ]] || [[ $flag_a == "2,1000" && $flag_r == 0 ]]; then
+    r=""
+fi
+
+function espaco() {
+    local temp_var=0
+    local dir="$1"
+    local space=0
+    if [[ ! -d "$dir" ]]; then #se não for um diretório
+        echo "Erro: Diretório inválido"
+        return 1
+    fi
+
+
+    dirs=()
+    while read -r -d '' directory; do
+        if [[ $? -ne 0 ]]; then
+            dict["$directory"]=-1
+            return 1
+        else
+            dirs+=("$directory")
+        fi
+    done < <(find "$dir" -mindepth 1 -maxdepth 1 -type d ! -name "*.*" -print0 2>/dev/null)
+
+    files=()
+    while read -r -d '' file; do
+        if [[ $? -ne 0 ]]; then
+            dict["$dir"]=-1
+            return 1
+        fi
+        files+=("$file")
+    done < <(find "$dir" -maxdepth 1 -type f -name "$flag_n" ! -newermt "@$flag_d" -print0 2>/dev/null)
+ 
+
+
+    #encontra os ficheiros em $dir com o nome a corresponder a $flag_n alterados não depois de $Flag_d
+    for j in "${files[@]}"; do #itera sobre a dict de ficheiros encontrados
+        space=$(du -b "$j" 2>/dev/null| awk '{print $1}' | grep -oE '[0-9.]+') #encontra o tamanho do ficheiro usando du
+        if [[ -z $space ]]; then
+            dict["$dir"]=-1
+            return 1
+        fi
+        if [[ $space -ge $flag_s ]] ; then #verifica se o tamanho do ficheiro encontra os requisitos de tamanho
+            total_var=$(( $total_var + $space )) #soma o espaço do ficheiro analisado ao total até agora
+        fi
+    done
+    for k in "${dirs[@]}"; do
+        temp_var=$total_var
+        total_var=0
+        espaco "$k"
+        if [[ $? -ne 0 ]];then
+            total_var=0
+        fi
+        total_var=$(( $temp_var + $total_var ))
+    done
+    dict["$dir"]=$total_var
+    return 0
+}
+
+function printer(){
+    counter=1
+    for key in "${!dict[@]}"; do
+        printf "%s %s\n" "${dict["$key"]}" "$key"
+    done | sort -k"$flag_a""$r" | while read -r line; do
+        if [[ $counter -gt $flag_l ]] && [[ $flag_l -ne 0 ]]; then
+            exit 0
+        fi
+        space=$(echo "$line" | awk '{print $1}')
+        if [[ $space == -1 ]]; then
+            space="NA"
+        fi
+        dir=$(echo "$line" | cut -d" " -f2-)
+        echo "$space $dir"
+        counter=$(( $counter + 1 ))
+    done #ordena a dict por ordem decrescente de tamanho e guarda os nomes dos diretórios ordenados    
+}
+
+#MAIN
+declare -A dict
+maindir=$(pwd)
+counting=0
+
+for l in "$@"; do
+    if [[ -d "$l" ]]; then
+        l=$(realpath --relative-to="$maindir" "$l")
+        total_var=0
+	    espaco "$l"
+    fi
+done
+
+if [[ "${#dict[@]}" == 0 ]]; then
+    echo "Nenhum diretório inserido."
+    exit 1
+fi
+
+echo "SIZE NAME $(date +%Y%m%d) $@"
+printer
---
+++ sop_tg1_113402_115243/spacerate.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+declare -A dictantigo
+declare -A dictnovo
+declare -A dictfinal
+
+
+flag_a="1,1n"
+flag_r=0
+input_novo=""
+input_antigo=""
+
+while getopts "ar" opt 2>/dev/null; do
+    case $opt in
+        a)
+            flag_a="2,1000"
+            ;;
+        r)
+            flag_r=1
+            ;;
+        \?)
+            echo "Invalid option!" >&2
+            exit 1
+            ;;
+    esac
+done
+
+r="r"
+if [[ $flag_a == "1,1n" && $flag_r == 1 ]] || [[ $flag_a == "2,1000" && $flag_r == 0 ]]; then 
+    r=""
+fi
+
+
+
+for arg in "$@"; do
+    if [ -f "$arg" ]; then
+        if [ -z "$input_novo" ]; then
+            input_novo="$arg"
+        elif [ -z "$input_antigo" ]; then
+            input_antigo="$arg"
+            break  # Encontrámos ambos os ficheiros nos argumentos
+        fi
+    fi
+done
+
+if [[ -z "$input_novo" ]] || [[ -z "$input_antigo" ]]; then
+    echo "Usage: $0 [-a] [-r] <input_novo> <input_antigo>"
+    exit 1
+fi
+
+if [ ! -r $input_antigo ]; then
+    echo "File $input_antigo nao tem permissao de leitura."
+    exit 1
+fi
+
+if [ ! -r $input_novo ]; then
+    echo "File $input_novo nao tem permissao de leitura."
+    exit 1
+fi
+
+first_line=true
+
+while read -r line
+do
+  if [ "$first_line" = true ]; then
+    first_line=false
+    continue  
+  fi
+
+  size=$(echo "$line" | cut -d\  -f1) #separa o tamanho do nome do diretório
+  name=$(echo "$line" | cut -d\  -f2-)
+
+  dictantigo["$name"]=$size #guarda o tamanho e o nome do diretório numa dict
+
+done < "$input_antigo"
+
+first_line=true
+
+while read -r line 
+do
+  if [ "$first_line" = true ]; then
+    first_line=false
+    continue  
+  fi
+
+  size=$(echo "$line" | cut -d\  -f1) #separa o tamanho do nome do diretório
+  name=$(echo "$line" | cut -d\  -f2-)
+
+  dictnovo["$name"]=$size #guarda o tamanho e o nome do diretório numa dict
+
+done < "$input_novo"
+
+for key in "${!dictnovo[@]}"; do #percorre a dict do diretório novo
+    if [[ ${dictantigo["$key"]} ]]; then #se o diretório já existia no diretório antigo
+        if [[ ${dictnovo["$key"]} == "NA" || ${dictantigo["$key"]} == "NA" ]]; then
+            dictfinal["$key"]="NA"
+        else
+            dictfinal["$key"]=$(( ${dictnovo["$key"]} - ${dictantigo["$key"]} )) #guarda a diferença de tamanho entre o diretório antigo e o novo
+        fi
+    
+    else #se o diretório é novo
+        dictfinal["$key NEW"]=${dictnovo["$key"]} #guarda o tamanho do diretório novo
+    fi
+done
+
+for key in "${!dictantigo[@]}"; do #percorre a dict do diretório antigo
+    if [[ ! ${dictnovo["$key"]} ]]; then #se o diretório foi removido
+        if [[ ${dictantigo["$key"]} == "NA" ]]; then
+            dictfinal["$key REMOVED"]="${dictantigo["$key"]}" #guarda o simétrico do tamanho do diretório antigo
+        else
+            dictfinal["$key REMOVED"]="-${dictantigo["$key"]}"
+        fi
+    fi
+done
+
+echo "SIZE NAME"
+for key in "${!dictfinal[@]}"; do #percorre a dict final
+    printf "%s %s\n" "${dictfinal["$key"]}" "$key" #imprime o tamanho e o nome do diretório
+done | sort -k"$flag_a$r" | while read -r line; do #
+    space=$(echo "$line" | awk '{print $1}') #
+    dir=$(echo "$line" | cut -d" " -f2-) #
+    printf "%s %s\n" "$space" "$dir" #
+done #ordena a dict por ordem decrescente de tamanho e guarda os nomes dos diretórios ordenados
